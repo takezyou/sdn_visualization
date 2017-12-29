e5bf6a805670d0f879b3ed7bc8dd4a0f7dc174f3
e145770@ie.u-ryukyu.ac.jp <akichan@ns.ie.u-ryukyu.ac.jp>
2017-12-30 06:12:32 +0900

    model add datapath

--- sdn_visualization/visualization/models.py
+++ sdn_visualization/visualization/models.py
@@ -18,3 +18,20 @@ class Vlans(models.Model):
     
     def __str__(self):
         return self.vlanid
+
+class Route(models.Model):
+    id = models.AutoField(primary_key=True)
+    start = models.CharField(max_length=200, null=True)
+    end = models.CharField(max_length=200, null=True)
+    route = models.TextField(null=True)
+    updated = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.start
+
+class Datapath(models.Model):
+    id = models.AutoField(primary_key=True)
+    route = models.TextField(null=True)
+
+    def __str__(self):
+        return self.id
